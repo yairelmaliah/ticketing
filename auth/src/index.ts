f96583fb354fb1bdddaf898d8e512a134df43a68
yairelmaliah <yairelmaliah319732@gmail.com>
2022-03-17 04:50:10 +0200

    test 2

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,11 +2,7 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
-<<<<<<< HEAD
   console.log('Starting up.asdasdasd..');
-=======
-  console.log('Starting up................');
->>>>>>> d2a0a8e7a24fb565c9a097882948c22cf856342b
   if (!process.env.JWT_KEY) throw new Error('JWT_KEY must be defined');
 
   if (!process.env.MONGO_URI) throw new Error('JWT_KEY must be defined');
