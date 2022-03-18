7e54680202a049050f0c46247b8ab8a748ceebad
yairelmaliah <yairelmaliah319732@gmail.com>
2022-03-18 04:58:54 +0200

    adding base_url env

--- client/pages/api/build-client.js
+++ client/pages/api/build-client.js
@@ -2,19 +2,9 @@ import axios from 'axios';
 
 export default ({ req }) => {
   if (typeof window === 'undefined') {
-    let baseURL;
-    if (
-      req.headers.host === 'www.ticketing-app-yair.xyz' ||
-      'ticketing-app-yair.xyz'
-    ) {
-      baseURL = 'http://www.ticketing-app-yair.xyz';
-    } else {
-      baseURL =
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local';
-    }
     // We are on the server side
     return axios.create({
-      baseURL,
+      baseURL: process.env.BASE_URL,
       headers: req.headers,
     });
   } else {
